dcb008059093fb9a1d7f3ebb6d30b1fc2a227bf2
SGUDestiny <panormov2006@gmail.com>
2024-09-09 21:55:56 +0400

    Mixins

--- src/main/java/destiny/fearthelight/common/daybreak/DaybreakOverworldEffects.java
+++ src/main/java/destiny/fearthelight/common/daybreak/DaybreakOverworldEffects.java
@@ -1,23 +1,23 @@
 package destiny.fearthelight.common.daybreak;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.Camera;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.DimensionSpecialEffects;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.phys.Vec3;
+import org.joml.Matrix4f;
 
-public class DaybreakOverworldEffects extends DimensionSpecialEffects {
+public class DaybreakOverworldEffects extends DimensionSpecialEffects.OverworldEffects {
     public static final ResourceLocation OVERWORLD_EFFECTS = new ResourceLocation("overworld");
 
-    public DaybreakOverworldEffects(float p_108866_, boolean p_108867_, SkyType p_108868_, boolean p_108869_, boolean p_108870_) {
-        super(p_108866_, p_108867_, p_108868_, p_108869_, p_108870_);
-    }
+    public DaybreakOverworldEffects(){}
 
     @Override
-    public Vec3 getBrightnessDependentFogColor(Vec3 biomeFogColor, float daylight) {
-        return null;
-    }
+    public boolean renderSky(ClientLevel level, int ticks, float partialTick, PoseStack poseStack, Camera camera, Matrix4f projectionMatrix, boolean isFoggy, Runnable setupFog) {
+        if(StellarViewConfig.replace_overworld.get())
+            return ViewCenters.renderViewCenterSky(level, ticks, partialTick, poseStack, camera, projectionMatrix, isFoggy, setupFog);
 
-    @Override
-    public boolean isFoggyAt(int x, int y) {
         return false;
     }
 
