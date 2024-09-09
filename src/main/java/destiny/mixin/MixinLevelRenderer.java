8b9d9b04b2fd804e99f0bd8219597fdaa76a9ef0
SGUDestiny <panormov2006@gmail.com>
2024-09-09 23:16:30 +0400

    Mixins

---
+++ src/main/java/destiny/mixin/MixinLevelRenderer.java
@@ -0,0 +1,26 @@
+package destiny.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import destiny.fearthelight.common.daybreak.DaybreakCapability;
+import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.resources.ResourceLocation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(LevelRenderer.class)
+abstract class MixinLevelRenderer {
+
+    @WrapOperation(
+            method = "renderSky",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderTexture(ILnet/minecraft/resources/ResourceLocation;)V",
+                    ordinal = 0
+            )
+    )
+    void setSunTexture(int i, ResourceLocation sunTexture, Operation<Void> original) {
+        if ()
+        original.call(i, sunTexture);
+    }
+}
