ca27fd5e6ba7bbbd0b987c1d83e7810236c43a1d
SGUDestiny <panormov2006@gmail.com>
2024-09-18 16:51:08 +0400

    Stuff

--- src/main/java/destiny/fearthelight/common/daybreak/DaybreakCapability.java
+++ src/main/java/destiny/fearthelight/common/daybreak/DaybreakCapability.java
@@ -2,22 +2,18 @@ package destiny.fearthelight.common.daybreak;
 
 import destiny.fearthelight.Config;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.common.util.INBTSerializable;
 
 public class DaybreakCapability implements INBTSerializable<CompoundTag> {
-    public static final String DAYBREAK_MODE = "daybreakMode";
     public static final String DAYBREAK_CHANCE = "daybreakChance";
     public static final String DAYBREAK_TIMER = "daybreakTimer";
     public static final String DAYBREAK_DAYS_LEFT = "daybreakDaysLeft";
 
-    public String daybreakMode = "";
     public double daybreakChance = 0.0;
     public int daybreakTimer = 0;
     public int daybreakDaysLeft = 0;
-    public int previousDay = 0;
+    public int previousDay = -1;
 
     public void tick(Level level) {
         if(level.isClientSide() || level.getServer() == null) {
@@ -34,14 +30,8 @@ public class DaybreakCapability implements INBTSerializable<CompoundTag> {
     }
 
     public void onLoad(Level level) {
-        if(previousDay == 0) {
+        if(previousDay == -1) {
             previousDay = getCurrentDay(level);
-        }
-
-        if (getDaybreakMode() == null) {
-            setDaybreakMode(Config.daybreakMode.toString());
-            setDaybreakChance(Config.daybreakStartingChance);
-            setDaybreakTimer(Config.daybreakTimer);
         }
     }
 
@@ -49,17 +39,24 @@ public class DaybreakCapability implements INBTSerializable<CompoundTag> {
     public void daybreakChanceCalc(Level level) {
         double additive = Config.daybreakAdditiveChance;
         int currentDay = getCurrentDay(level);
-        double chance = getDaybreakChance();
+        double chance = Config.daybreakStartingChance;
 
+        System.out.println("=================");
+        System.out.println("Mode: " + Config.daybreakMode);
+        System.out.println("Chance: " + daybreakChance);
+        System.out.println("Additive: " + additive);
+        System.out.println("Timer: " + daybreakTimer);
+        System.out.println("-----------");
         System.out.println("Current day: " + currentDay);
         System.out.println("Previous day: " + previousDay);
-        System.out.println("Chance: " + chance);
-        System.out.println("Additive: " + additive);
+        System.out.println("Days left: " + daybreakDaysLeft);
+        System.out.println("=================");
 
-        setDaybreakChance(chance + (additive * (currentDay - 1)));
+        daybreakChance = chance + (additive * currentDay);
 
         if(currentDay > previousDay) {
-            if(getDaybreakChance() > level.random.nextDouble() || getDaybreakChance() >= 1) {
+            System.out.println("!!! Random chance rolled !!!");
+            if(daybreakChance > level.random.nextDouble() || daybreakChance >= 1) {
                 daybreakTrigger(level);
             }
             previousDay = currentDay;
@@ -67,10 +64,10 @@ public class DaybreakCapability implements INBTSerializable<CompoundTag> {
     }
 
     public void daybreakCountdownCalc(Level level) {
-        int timer = getDaybreakTimer();
+        int timer = Config.daybreakTimer;
 
         if(timer > 0) {
-            setDaybreakTimer(timer - 1);
+            daybreakTimer = timer - 1;
         } else {
             daybreakTrigger(level);
         }
@@ -79,60 +76,26 @@ public class DaybreakCapability implements INBTSerializable<CompoundTag> {
     public void daybreakTrigger(Level level) {
         int lengthMultiplier = Config.daybreakLengthMultiplier;
 
-        setDaybreakDaysLeft((int) (lengthMultiplier * level.random.nextDouble()));
-    }
-
-    public String getDaybreakMode() {
-        return daybreakMode;
-    }
-    public double getDaybreakChance() {
-        return daybreakChance;
-    }
-    public int getDaybreakTimer() {
-        return daybreakTimer;
-    }
-    public int getDaybreakDaysLeft() {
-        return daybreakDaysLeft;
-    }
-
-    public void setDaybreakMode(String daybreakMode) {
-        this.daybreakMode = daybreakMode;
-    }
-    public void setDaybreakChance(double daybreakChance) {
-        this.daybreakChance = daybreakChance;
-    }
-    public void setDaybreakTimer(int daybreakTimer) {
-        this.daybreakTimer = daybreakTimer;
-    }
-    public void setDaybreakDaysLeft(int daybreakDaysLeft) {
-        this.daybreakDaysLeft = daybreakDaysLeft;
+        daybreakDaysLeft = (int) (lengthMultiplier * level.random.nextDouble());
     }
 
     public int getCurrentDay(Level level) {
-        return (int) level.getDayTime() / 24000;
+        return (int) (level.getDayTime() / 24000 % 2147483647L);
     }
 
     @Override
     public CompoundTag serializeNBT() {
         CompoundTag tag = new CompoundTag();
 
-        tag.putString(DAYBREAK_MODE, daybreakMode);
         tag.putDouble(DAYBREAK_CHANCE, daybreakChance);
         tag.putInt(DAYBREAK_TIMER, daybreakTimer);
         tag.putInt(DAYBREAK_DAYS_LEFT, daybreakDaysLeft);
-
-        System.out.println("=================");
-        System.out.println("Mode: " + daybreakMode);
-        System.out.println("Chance: " + daybreakChance);
-        System.out.println("Timer: " + daybreakTimer);
-        System.out.println("Days left: " + daybreakDaysLeft);
 
         return tag;
     }
 
     @Override
     public void deserializeNBT(CompoundTag tag) {
-        this.daybreakMode = tag.getString(DAYBREAK_MODE);
         this.daybreakChance = tag.getDouble(DAYBREAK_CHANCE);
         this.daybreakTimer = tag.getInt(DAYBREAK_TIMER);
         this.daybreakDaysLeft = tag.getInt(DAYBREAK_DAYS_LEFT);
